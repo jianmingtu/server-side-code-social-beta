19e2b7c2189f709e789446fae8726da1bcd49968
jianmingtu <tu_jm@yahoo.com>
2021-03-15 12:22:00 -0700

    #8 -  add createComment and getPostComment to backend

--- nodejs/socialCafeDatabase.js
+++ nodejs/socialCafeDatabase.js
@@ -1,12 +1,9 @@
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
+const MONGODB_URI = `mongodb+srv://team8:team8@cluster0.kgzz2.mongodb.net/socialCafe?retryWrites=true&w=majority`;;
 
-const MONGODB_URI = `mongodb+srv://xxxx:xxxx@cluster0.kgzz2.mongodb.net/socialCafe?retryWrites=true&w=majority`;
 
-let cachedDb = null;
-async function connectToDatabase() {
-    if (cachedDb) {
-        return cachedDb;
-    }
+module.exports = async (event, context) => {
+
     // Connect to our MongoDB database hosted on MongoDB Atlas
     const client = await MongoClient(MONGODB_URI, {
         useUnifiedTopology: true,
@@ -14,26 +11,53 @@ async function connectToDatabase() {
     }).connect();
     // Specify which database we want to use
     const db = client.db('socialCafe');
-    cachedDb = db;
-    return db;
-}
-module.exports = async (event, context) => {
-
-    // Connect to mongodb database
-    const db = await connectToDatabase();
-
+    
     async function createPost({user, body})  {     
         return await db.collection('Posts').insertOne({
             ...body,
             totalLikes: 0,
             totalComments: 0,
             timestamp: Date.now(),
-            ...user,            
+            user,            
         })
     }
 
+    async function getPostComments({ postId }) {
+        return await db.comments.find({ postId: ObjectId(postId) }).sort({ "timestamp": -1 }).toArray()
+    }
+
+     async function createComment({user, body }) {
+
+        const {postId, comment} = body
+        
+        const session = client.startSession()
+        session.startTransaction()
+        try {
+            
+            //  insert comment
+            const result = await db.comments.insertOne({
+                ...comment,
+                postId: ObjectId(postId),
+                ...user,  
+                timestamp: Date.now()
+            })
+    
+            //  increase total
+              await db.posts.findOneAndUpdate({ "_id": ObjectId(postId) }, {$inc: { "totalComments": 1 }})
+              
+              await session.commitTransaction()
+              
+              return result.ops[0]
+        } catch (error) {
+              await session.abortTransaction()
+              throw error
+        } finally {
+            await session.endSession()
+        }
+    }
 
     return {
-        createPost
+        createPost,
+        createComment,
     }
 }
--- test.js
+++ test.js
@@ -2,30 +2,58 @@
 //  Local express test
 //
 const express = require('express')
-const mongoDB = require('./nodejs/socialCafeDatabase')
+const socialCafeDB = require('./nodejs/socialCafeDatabase')
+const {authorize} = require('./jwt')
 
 const app = express()
 app.use(express.json())
 
 
-// const CreatePostMongoDB = async ({body, user}) => {
+// app.get('/posts', async (req, res) => {
 //     try {
-//         const Post = await mongoDB.createPost();      
-//         return {
-//             statusCode: 200,
-//             body: JSON.stringify({
-//                 posts: posts
-//             }),
-//         };
-//     } catch (err) {
-//         return {
-//             statusCode: 500,
-//             body: JSON.stringify({
-//                 errorMsg: `Error while creating a user: ${err}`,
-//             }),
-//         };
+//     const posts = await GetPostsMongoDB()
+//     res.send({posts})
+//     } catch (error) {
+//         console.error(error)
+//         res.send({error: error.message})
 //     }
-// }
+// })
+
+
+app.get('/posts', authorize, async (req, res) => {
+    
+    // context.callbackWaitsForEmptyEventLoop = false;
+        
+    try {
+        
+        const body = req.body
+        const user = req.user
+        
+        const db = await socialCafeDB()
+        
+        const post = await db.createPost({user, body})
+        
+        return {
+            statusCode: 200,
+            headers:{ 'Access-Control-Allow-Origin' : '*' },
+            body: JSON.stringify({
+                post: post.ops[0]
+            }),
+        };
+    } catch (err) {
+        return {
+            statusCode: 500,
+            headers:{ 'Access-Control-Allow-Origin' : '*' },
+            body: JSON.stringify({
+                errorMsg: `Error while creating a user: ${err}`,
+            }),
+        };
+    }
+};
+
+
+
+
 
 // // POST /api/posts
 // router.post('/', authorize, async (req, res) => {
@@ -33,6 +61,10 @@ app.use(express.json())
 //     const user = req.user
 //     await CreatePostMongoDB({body, user})
 // })
+
+
+
+
 
 // app.get('/posts', async (req, res) => {
 //     try {
